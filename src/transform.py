2fbf1b8fca0ce5385d3f0786f44788dbc625b744
riguel Jaffet ccohuanqui huaman <017200640f@gmail.com>
2026-04-10 12:19:57 -0500

    transformación de los datos v1 clean missing values

--- src/transform.py
+++ src/transform.py
@@ -1,8 +1,17 @@
-def clean_missing_values(raw_df):
-    print("clean_missing_values")
+import pandas as pd
 
-def normalize_columns(clean_df):
-    print("normalize_columns")
+def clean_missing_values(df: pd.DataFrame) -> pd.DataFrame:
+    """Rellena o elimina valores nulos"""
 
-def generate_features(normalized_df):
-    print("generate_features")
+    # hacemos una copia para no alterar el dataframe original
+    clean_df = df.copy()
+
+    # rellenamos valores nulos en columnas de texto con 'desconocido'
+    cols_to_fill = ['director', 'cast', 'country']
+    for col in cols_to_fill:
+        clean_df[col] = clean_df[col].fillna('Desconocido')
+
+    # si no hay fecha o rating se eliminaran por completo
+    clean_df = clean_df.dropna(subset=['date_added', 'rating'])
+
+    return clean_df
