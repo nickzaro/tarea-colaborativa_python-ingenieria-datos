cccec91c3dec6f6292e351d5a6c54917f9262ab5
Victor Hugo Giraldez Taco <victor1999.work@gmail.com>
2026-04-10 12:44:59 -0500

    feat(vis): integrar gráfico de violín para antigüedad de lanzamientos

--- src/visualize.py
+++ src/visualize.py
@@ -21,8 +21,6 @@ def plot_top_ratings(df):
     plt.tight_layout()
     plt.savefig('output/1_top_ratings.png')
     plt.close()
-
-
 def plot_type_proportions(df):
     plt.figure(figsize=(8, 8))
     counts = df['type'].value_counts()
@@ -31,5 +29,16 @@ def plot_type_proportions(df):
     plt.legend(counts.index, title="Tipo de Contenido", loc="upper left", bbox_to_anchor=(1, 0.9))
     plt.tight_layout()
     plt.savefig('output/2_proporcion_peliculas_series.png')
+    plt.close()
+def plot_violin_releases(df):
+    plt.figure(figsize=(10, 6))
+    sns.violinplot(data=df[df['release_year'] > 1990], x='type', y='release_year', 
+                   hue='type', palette='muted', legend=True, dodge=False)
+    plt.title('Distribución de Lanzamientos (Violin)')
+    plt.xlabel('Tipo de Contenido')
+    plt.ylabel('Año de Lanzamiento Original')
+    plt.legend(title="Clasificación")
+    plt.tight_layout()
+    plt.savefig('output/3_distribucion_lanzamientos_violin.png')
     plt.close()
 
