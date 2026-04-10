b6fe974a27ffe3b27fdc5c5b6a12815da3485d18
Alberto Zárate <nickzaro@gmail.com>
2026-04-10 16:31:57 -0500

    Fix: Agregando el guardado a csv

--- src/load.py
+++ src/load.py
@@ -1,4 +1,4 @@
 
 def save_data(df, name_dataframe):
-    print(f"Guardando el DataFrame que contiene {len(df)}, como {name_dataframe}")
+    print(f"Guardando el DataFrame que contiene {len(df)} filas, como {name_dataframe}")
     df.to_csv(name_dataframe, index=False, encoding='utf-8')    
