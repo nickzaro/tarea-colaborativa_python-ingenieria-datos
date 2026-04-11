3af5b923cb2430117ac8c93c9b64816f0b97f00c
Victor Hugo Giraldez Taco <victor1999.work@gmail.com>
2026-04-10 19:55:42 -0500

    feat(load): implementar compuerta de calidad para validacion pura de datos

--- src/load.py
+++ src/load.py
@@ -1,4 +1,17 @@
+import pandas as pd
+import sqlite3
+import json
+from datetime import datetime
+import os
 
-def save_data(df, name_dataframe):
-    print(f"Guardando el DataFrame que contiene {len(df)} filas, como {name_dataframe}")
-    df.to_csv(name_dataframe, index=False, encoding='utf-8')    
+def execute_quality_gate(df: pd.DataFrame) -> bool:
+
+    if df.empty:
+        print("[FALLO] Calidad de Datos Fallida: El DataFrame esta vacio.")
+        return False
+    
+    if df['show_id'].isnull().any():
+        print("[FALLO] Calidad de Datos Fallida: Se encontraron IDs primarios nulos.")
+        return False
+        
+    return True
