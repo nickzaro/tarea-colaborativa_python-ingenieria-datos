b272b8aaf9b204a69a6ddfa738296b45a912b5c9
riguel jaffet ccohuanqui huaman <017200640f@uandina.edu.pe>
2026-04-17 20:59:40 -0500

    characterize-apache v1

--- src/characterize.py
+++ src/characterize.py
@@ -8,24 +8,6 @@ def generate_features(normalized_df):
     """
     # Copia de seguridad para evitar advertencias de SettingWithCopy
     df_features = normalized_df.copy()
-
-    # Extraemos año y nombre del mes de la columna que ya transformamos a datetime
-    df_features['year_added'] = df_features['date_added'].dt.year
-    df_features['month_added'] = df_features['date_added'].dt.month_name()
-    
-    # Extraemos solo el primer género listado para simplificar visualizaciones
-    df_features["main_genre"] = df_features["listed_in"].apply(lambda x: x.split(",")[0])
-
-    # Extraemos el valor numérico de la columna 'duration'
-    # "90 min" -> 90.0 | "2 Seasons" -> 2.0
-    df_features['duration_num'] = df_features['duration'].str.extract('(\d+)').astype(float)
-    
-    # Creamos columnas específicas para no mezclar minutos con temporadas
-    df_features["movie_duration_min"] = df_features.apply(
-    lambda x: x["duration_num"] if x["type"] =="Movie" else None, axis=1
-)
-    df_features["tv_show_seasons"] = df_features.apply(
-    lambda x: x["duration_num"] if x["type"] =="Tv show" else None, axis=1
 )
 
     # Diferencia entre el año actual y el año de estreno original
